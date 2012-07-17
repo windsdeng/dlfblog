f6546e8b57f7cc3c7e899334b18b050a52caf047
winds <windsdeng@hotmail.com>
2012-07-18 02:26:32 +0800

    Update master

--- protected/components/views/recentComments.php
+++ protected/components/views/recentComments.php
@@ -5,27 +5,3 @@
 	</li>
 	<?php endforeach; ?>
 </ul>
-
-<script type="text/javascript"><!--
-google_ad_client = "ca-pub-2584272689913259";
-/* 最近的评论180&#42;90 */
-google_ad_slot = "5658453915";
-google_ad_width = 180;
-google_ad_height = 90;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-
-<script type="text/javascript"><!--
-google_ad_client = "ca-pub-2584272689913259";
-/* 最近的评论120&#42;90 */
-google_ad_slot = "2339865623";
-google_ad_width = 120;
-google_ad_height = 90;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
