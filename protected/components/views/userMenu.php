74618632c913cf47cd43c727d65ce4edef5dae2e
windsdeng <windsdeng@hotmail.com>
2012-09-11 14:53:14 +0800

    update admin

--- protected/components/views/userMenu.php
+++ protected/components/views/userMenu.php
@@ -2,6 +2,7 @@
 	<li><?php if(Yii::app()->user->isGuest) echo CHtml::link('Login',array('site/login')); ?></li>
 	<li><?php echo CHtml::link('Create New Post',array('post/create')); ?></li>
 	<li><?php echo CHtml::link('Manage Posts',array('post/admin')); ?></li>
+	<li><?php echo CHtml::link('Manage Backend','dlf-admin.php'); ?></li>
 	<li><?php echo CHtml::link('Approve Comments',array('comment/index')) . ' (' . Comment::model()->pendingCommentCount . ')'; ?></li>
 	<li><?php if(!Yii::app()->user->isGuest) echo CHtml::link(Yii::app()->user->name.'(Logout)',array('site/logout')); ?></li>
 </ul>
