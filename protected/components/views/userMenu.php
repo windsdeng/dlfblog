f66c840428682a3b7ea27858f59b4ac1683886f5
windsdeng <windsdeng@hotmail.com>
2012-09-23 18:40:13 +0800

    update url

--- protected/components/views/userMenu.php
+++ protected/components/views/userMenu.php
@@ -5,7 +5,7 @@
 	<?php if(!Yii::app()->user->isGuest): ?>
     <li><?php echo CHtml::link('Create New Post',array('post/create')); ?></li>
 	<li><?php echo CHtml::link('Manage Posts',array('post/admin')); ?></li>
-	<li><?php echo CHtml::link('Manage Backend','dlf-admin.php'); ?></li>
+	<li><?php echo CHtml::link('Manage Backend',Yii::app()->createUrl('dlf-admin.php')); ?></li>
 	<li><?php echo CHtml::link('Approve Comments',array('comment/index')) . ' (' . Comment::model()->pendingCommentCount . ')'; ?></li>
 	<li><?php if(!Yii::app()->user->isGuest) echo CHtml::link(Yii::app()->user->name.'(Logout)',array('site/logout')); ?></li>
     <?php endif; ?>
