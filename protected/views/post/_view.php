29410ae1c110a3ef898fc23c42e876e784eee03f
windsdeng <windsdeng@hotmail.com>
2012-09-24 14:51:06 +0800

    update share

--- protected/views/post/_view.php
+++ protected/views/post/_view.php
@@ -4,6 +4,22 @@
 	</div>
 	<div class="author">
 		posted by <?php echo $data->author->username . ' on ' . date('F j, Y',$data->created); ?>
+		<!-- Baidu Button BEGIN -->
+	    <div id="bdshare" class="bdshare_t bds_tools get-codes-bdshare">
+	        <a class="bds_qzone"></a>
+	        <a class="bds_tsina"></a>
+	        <a class="bds_tqq"></a>
+	        <a class="bds_renren"></a>
+	        <span class="bds_more">更多</span>
+			<a class="shareCount"></a>
+	    </div>
+		<script type="text/javascript" id="bdshare_js" data="type=tools&amp;uid=138127" ></script>
+		<script type="text/javascript" id="bdshell_js"></script>
+		<script type="text/javascript">
+			document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + new Date().getHours();
+		</script>
+		<!-- Baidu Button END -->
+		<div class="clear"></div>
 	</div>
 	<div class="content">
 		<?php
